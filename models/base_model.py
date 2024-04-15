b337b795e3508dd4d02b9a3544680d012f5bfaf1
peter kateeba <petrkat8ba@gmail.com>
2024-04-15 07:23:06 -0700

    update

--- models/base_model.py
+++ models/base_model.py
@@ -10,14 +10,15 @@ class BaseModel:
         self.updated_at = self.created_at
 
     def __str__(self):
-        return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
+        class_name = self.__class__.__name__
+        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
 
     def save(self):
         self.updated_at = datetime.now()
 
     def to_dict(self):
         obj_dict = self.__dict__.copy()
-        obj_dict['__class__'] = type(self).__name__
+        obj_dict['__class__'] = self.__class__.__name__
         obj_dict['created_at'] = self.created_at.isoformat()
         obj_dict['updated_at'] = self.updated_at.isoformat()
         return obj_dict
